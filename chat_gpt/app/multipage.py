f54ccd6139cae462b161221eda5f656cdfa1da61
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2023-11-02 12:47:01 +0100

    Stay on new page when it is created
    The previous behaviour was to go back to page 0.

--- chat_gpt/app/multipage.py
+++ chat_gpt/app/multipage.py
@@ -17,6 +17,8 @@ class MultiPage:
     def __init__(self) -> None:
         """Constructor class to generate a list which will store all our applications as an instance variable."""
         self.pages = {}
+        # Keep track of which page we're on, so we remain in it when adding a new page
+        self.selected_chat_index = None
 
     def add_page(self, page_id, title, func) -> None:
         """Class Method to Add pages to the project
@@ -27,16 +29,17 @@ class MultiPage:
         """
 
         self.pages[page_id] = {"title": title, "function": func}
+        # Signal to `run` taht we should move to the newly added page
+        self.selected_chat_index = list(self.pages.keys()).index(page_id)
 
     def run(self):
         # Drodown to select the page to run
-        id_and_page = st.sidebar.selectbox(
+        if id_and_page := st.sidebar.selectbox(
             label="Select Chat",
             options=self.pages.items(),
             format_func=lambda id_and_page: id_and_page[1]["title"],
-        )
-
-        # run the app function
-        if id_and_page is not None:
+            index=self.selected_chat_index,
+        ):
+            # run the app function
             page_id, page = id_and_page
             page["function"](page_id=page_id)
