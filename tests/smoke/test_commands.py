e514e85dcd736da4e489be6ce0b7afa5660343e5
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2023-11-10 11:07:13 +0100

    Some fixed in tests

--- tests/smoke/test_commands.py
+++ tests/smoke/test_commands.py
@@ -7,14 +7,13 @@ from gpt_buddy_bot.argparse_wrapper import get_parsed_args
 @pytest.mark.usefixtures("_input_builtin_mocker")
 @pytest.mark.parametrize("user_input", ["Hi!", ""], ids=["regular-input", "empty-input"])
 def test_terminal_command(cli_args_overrides):
-    args = [*["terminal", "--report-accounting-when-done"], *cli_args_overrides]
+    args = ["terminal", "--report-accounting-when-done", *cli_args_overrides]
     args = list(dict.fromkeys(args))
     main(args)
 
 
 def test_accounting_command():
-    args = ["accounting"]
-    main(args)
+    main(["accounting"])
 
 
 def test_default_command(mocker):
