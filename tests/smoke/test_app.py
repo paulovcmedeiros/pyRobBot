d547d108373835d415ad77b77c03a928779d20e6
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2024-02-24 22:31:40 +0100

    Fix to tests due to AssistantResponseChunk

--- tests/smoke/test_app.py
+++ tests/smoke/test_app.py
@@ -1,11 +1,11 @@
 from pyrobbot.app import app
 
 
-def test_app(mocker, default_chat_configs):
+def test_app(mocker, default_voice_chat_configs):
     mocker.patch("streamlit.session_state", {})
     mocker.patch("streamlit.chat_input", return_value="foobar")
     mocker.patch(
-        "pyrobbot.chat_configs.ChatOptions.from_file",
-        return_value=default_chat_configs,
+        "pyrobbot.chat_configs.VoiceChatConfigs.from_file",
+        return_value=default_voice_chat_configs,
     )
     app.run_app()
