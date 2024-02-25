f54024adf0ae60e8906609c89719d833f8e69a63
Paulo V C Medeiros <paulo.medeiros@smhi.se>
2024-02-25 13:57:23 +0100

    Fixes to web app test

--- tests/smoke/test_app.py
+++ tests/smoke/test_app.py
@@ -2,7 +2,14 @@ from pyrobbot.app import app
 
 
 def test_app(mocker, default_voice_chat_configs):
-    mocker.patch("streamlit.session_state", {})
+    class MockAttrDict(dict):
+        def __getattr__(self, attr):
+            return self.get(attr, mocker.MagicMock())
+
+        def __setattr__(self, attr, value):
+            self[attr] = value
+
+    mocker.patch("streamlit.session_state", MockAttrDict())
     mocker.patch("streamlit.chat_input", return_value="foobar")
     mocker.patch(
         "pyrobbot.chat_configs.VoiceChatConfigs.from_file",
